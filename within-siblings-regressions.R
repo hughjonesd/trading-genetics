280de7c0bced7e0df96c3862d12dc0b741d36c58
David Hugh-Jones <davidhughjones@gmail.com>
2021-03-13 11:29:01 +0000

    add notes; add matching papers; add within siblings regression script

---
+++ within-siblings-regressions.R
@@ -0,0 +1,44 @@
+
+
+fml_bo_psea_fixef <- list()
+
+fml_bo_psea_fixef[[1]] <- EA3.y ~ birth_order.x | n_sibs.x + sib_group.x
+fml_bo_psea_fixef[[2]] <- EA3.y ~ birth_order.x + EA3.x |
+                            n_sibs.x + sib_group.x
+  
+fml_bo_psea_fixef[[3]] <- EA3.y ~ birth_order.x + EA3.x + 
+                            moth_age_birth.x | n_sibs.x + sib_group.x
+
+fml_bo_psea_fixef %<>% map(Formula::as.Formula)
+
+
+mod_bo_psea_fixef <- lapply(fml_bo_psea_fixef, fixest::feols, 
+                 data = mf_pairs_reg
+               )
+
+
+huxreg(mod_bo_psea_fixef, 
+         coefs = c(
+           "Birth order"           = "birth_order.x", 
+           "Own PSEA"              = "EA3.x",
+           "Mother's age at birth" = "moth_age_birth.x"
+         ),
+         statistics = c(
+           "N"  = "nobs", 
+           "R2" = "r.squared"
+          ),
+         stars = my_stars,
+         note = "{stars}. Clusters: sib group, family size",
+         tidy_args = list(
+           conf.int = FALSE, 
+           cluster  = list(mf_pairs_reg$sib_group.x, mf_pairs_reg$n_sibs.x)
+         )
+       ) %>% 
+       insert_row(after = 7, "Family size dummies", rep("Yes", 3)) %>% 
+       insert_row(after = 8, "Sibling group dummies", "Yes", "Yes", "Yes") %>% 
+       set_bottom_border(8, everywhere, 0) %>% 
+       set_number_format(2:7, -1, 4) %>% 
+       set_width(0.5) %>% 
+       set_position("left") %>% 
+       set_caption("Regressions of spouse PSEA on birth order: within-family")
+
